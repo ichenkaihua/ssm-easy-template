5dc27eac7702119ce3a9f4c8a653d91c4ecb8534
=ichenkaihua <=admin@chenkaihua.com>
2015-12-15 10:26:56 +0800

    添加属性设置

---
+++ db/migration/V1_1_1__init_user_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `user` (
+  `id`                    int(11) NOT NULL AUTO_INCREMENT,
+  `name`                  char(40) DEFAULT NULL,
+  `password`              char(50) DEFAULT NULL,
+ CONSTRAINT PRIMARY KEY (`id`)
+) ;
+
+INSERT INTO `user` VALUES (1,'chenkaihua',NULL);
+INSERT INTO `user` VALUES   (2,'test_user',NULL);
+INSERT INTO `user` VALUES (3,'陈开华','mypassword');
+INSERT INTO `user` VALUES (6,'myname','mypass');
+INSERT INTO `user` VALUES (7,'chenhua','chss');
+
+
